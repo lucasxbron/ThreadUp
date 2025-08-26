bd3c9a712e8854b5992abcf2de6e4fefa8b1ffd3
Lucas <lucasbron@mailbox.org>
2025-08-26 13:31:14 +0200

    feat: add JWT utility functions for token creation and verification

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+import createHttpError from "http-errors";
+import { JwtPayload } from "../types/jwt.js";
+
+const secret = config.JWT_SECRET;
+
+export function createToken(payload: JwtPayload): string {
+  try {
+    if (!secret) {
+      throw createHttpError(500, "JWT was not set");
+    }
+    
+    const token = jwt.sign(payload, secret, { expiresIn: "900s" }); // 15 minutes
+    return token;
+  } catch (error) {
+    throw createHttpError(500, "Token could not be created");
+  }
+}
+
+export function verifyJWT(token: string): JwtPayload {
+  try {
+    if (!secret) {
+      throw createHttpError(500, "JWT_SECRET not set");
+    }
+    
+    const payload = jwt.verify(token, secret) as JwtPayload;
+    return payload;
+  } catch (error) {
+    throw createHttpError(401, "Token invalid");
+  }
+}
