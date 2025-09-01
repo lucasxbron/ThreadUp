af5dc173b19cecbe22ad5ff99956e75c00be87db
Lucas <lucasbron@mailbox.org>
2025-09-01 08:44:41 +0200

    refactor: reorganize imports and streamline upload route handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
-import { connect } from "./utils/db.js";
 import express, { Request, Response, NextFunction } from "express";
+import { connect } from "./utils/db.js";
 import config from "./config/config.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { errorHandling } from "./middleware/errorHandling.js";
 import authRoutes from "./routes/authRouter.js";
 import postRoutes from "./routes/postRouter.js";
 import commentRoutes from "./routes/commentRouter.js";
 import likeRoutes from "./routes/likeRouter.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import { upload } from "./middleware/multer.js";
+import uploadRoutes from "./routes/uploadRouter.js";
 
 const app = express();
 
@@ -23,32 +23,15 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/uploads", express.static("public/uploads"));
-
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
-
-app.post(
-  "/api/upload",
-  upload.single("profileImage"),
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      if (!req.file) {
-        throw new Error("No file uploaded");
-      }
-      const fileUrl = `http://localhost:${config.PORT}/uploads/${req.file.filename}`;
-      res.status(200).json({ message: "Upload successful", url: fileUrl });
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+app.use("/api/upload", uploadRoutes);
 
 app.use(errorHandling);
 
 connect();
 app.listen(config.PORT, () =>
-  console.log(`Server running at http://localhost:${config.PORT}`)
+  console.log(`Server running on http://localhost:${config.PORT}`)
 );
